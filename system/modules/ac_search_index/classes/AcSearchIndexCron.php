c7ea485ccd9d2a101c59cb05fcd74e62dc5cd937
Leo Unglaub <leo.unglaub@2create.at>
2014-02-22 14:09:36 +0100

    Update the class AcSearchIndexCron to Contao 3.2
    Now the class AcSearchIndexCron uses the following new
    features:
    
     * Database::getInstance()
     * PageModel
    
    Signed-off-by: Leo Unglaub <leo.unglaub@2create.at>

--- system/modules/ac_search_index/classes/AcSearchIndexCron.php
+++ system/modules/ac_search_index/classes/AcSearchIndexCron.php
@@ -13,36 +13,34 @@
 
 /**
  * Class AcSearchIndexCron
- * Provide methods to build the search index
- *
- * @copyright  Leo Unglaub 2012
- * @author     Leo Unglaub <leo@leo-unglaub.net>
- * @package    ac_search_index
- * @license    LGPL
+ * Provide methods to manipulate the search index
  */
 class AcSearchIndexCron extends Controller
 {
 	/**
-	 * Update all keywords in the search index where the root id is missing
+	 * Update all keywords in the search index where the root id is
+	 * missing.
 	 *
-	 * @param int $intLimit
-	 * @return void
+	 * @param	int		$intLimit	The max amount of entries per run.
+	 * @return	void
 	 */
 	public function setMissingRootIds($intLimit=50)
 	{
-		$this->import('Database');
+		$objDb = Database::getInstance();
 
 		// get all entry's witch have no rootId
-		$objSearchIndex = $this->Database->query('SELECT DISTINCT s.pid as pageId FROM tl_search s JOIN tl_search_index i ON i.pid=s.id WHERE i.rootPage=0 LIMIT ' . $intLimit);
+		$objSearchIndex = $objDb->prepare('SELECT DISTINCT s.pid as pageId FROM tl_search s JOIN tl_search_index i ON i.pid=s.id WHERE i.rootPage=0')
+								->limit($intLimit)
+								->execute();
 
 		while ($objSearchIndex->next())
 		{
 			// get the root id of the current site
-			$intRootId = $this->getPageDetails($objSearchIndex->pageId)->rootId;
+			$objPage = PageModel::findWithDetails($objSearchIndex->pageId);
 
 			// store the root id in the search index
-			$this->Database->prepare('UPDATE tl_search_index i JOIN tl_search s ON i.pid=s.id SET i.rootPage=? WHERE s.pid=?')
-						   ->execute($intRootId, $objSearchIndex->pageId);
+			$objDb->prepare('UPDATE tl_search_index i JOIN tl_search s ON i.pid=s.id SET i.rootPage=? WHERE s.pid=?')
+				  ->execute($objPage->rootId, $objSearchIndex->pageId);
 		}
 	}
 }
